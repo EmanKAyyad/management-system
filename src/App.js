078b17123348d87f55fa912f295d7078bab03974
Eman Khalifa <eman.khalifa@softxpert.com>
2021-01-16 14:27:49 +0200

    fix transition behaviour

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './scss/App.scss';
 import Sidebar from './components/shared/Sidebar/Sidebar';
 import Header from './components/shared/Header/Header';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function App() {
+function App(props) {
+
+  const expandedPanel = {
+    flex: 1
+  };
+
+  const condensedPanel = {
+    width: 'calc(100% - 242px)'
+  };
+
   return (
     <BrowserRouter>
       <div className="app-container">
@@ -16,4 +26,9 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    panelExpanded: !state.openSideBarFlag
+  }
+}
+export default connect(mapStateToProps)(App);
