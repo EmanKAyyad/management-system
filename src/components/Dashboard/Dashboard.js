c41cadfacf92b5d74be6c59e0c06d59699f19ce1
Eman Khalifa <eman.khalifa@softxpert.com>
2021-01-28 18:19:45 +0200

    lazy loading

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './dashboard.scss';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import FuelHistory from './FuelHistory/FuelHistory';
+import asyncComponent from '../../hoc/asyncComponent';
+
+
+const asyncOperatingCost = asyncComponent(() => import('./OperatingCost/OperatingCost'));
 
 const Dashboard = () => {
     return <React.Fragment>
         <Switch>
             <Route path="/fuel-history" component={FuelHistory}></Route>
+            <Route path="/operating-cost" component={asyncOperatingCost}></Route>
             <Redirect from="/" to="/fuel-history"></Redirect>
         </Switch>
     </React.Fragment >
---
+++ src/components/Dashboard/OperatingCost/OperatingCost.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class OperatingCost extends Component {
+
+    render() {
+        return <h1>Operating Cost</h1>
+    }
+}
+
+export default OperatingCost;
